95f2befef91e18b9a9439b80be6a1364e2fdc0ba
Inikon <ZhanZS@mail.ru>
2022-04-15 23:08:23 +0300

    add popup

--- src/app.ts
+++ src/app.ts
@@ -17,14 +17,23 @@ class DataItem implements IData{
     if (typeof DataItemDTO.userId != "number"){
       throw new Error('User id is not number');
     }
+    this.userId = DataItemDTO.userId;
+
     if (typeof DataItemDTO.id != "number"){
       throw new Error('Item id is not number');
     }
+    this.id = DataItemDTO.id;
+
     if (typeof DataItemDTO.title != "string"){
       this.title = "Unnamed";
+    } else {
+      this.title = DataItemDTO.title;
     }
+    
     if (typeof DataItemDTO.body != "string"){
       this.body = "Empty body";
+    } else {
+      this.body = DataItemDTO.body;
     }
   }
 }
@@ -41,7 +50,7 @@ export class DataModel{
       if (!Array.isArray(res)){
         throw new Error('Data loading error, result is not array');
       }
-      this.data = res;
+      this.data = res.map(it=> new DataItem(it));
       return this;
     });
   }
@@ -53,13 +62,21 @@ class DataItemView extends Control{
   private title: Control<HTMLElement>;
   private body: Control<HTMLElement>;
 
+  public onOpen: ()=>void;
+
   constructor(parentNode: HTMLElement){
     super(parentNode);
+    const dataContainer = new Control(this.node);
+    this.id = new Control(dataContainer.node);
+    this.userId = new Control(dataContainer.node);
+    this.title = new Control(dataContainer.node);
+    this.body = new Control(dataContainer.node);
 
-    this.id = new Control(this.node);
-    this.userId = new Control(this.node);
-    this.title = new Control(this.node);
-    this.body = new Control(this.node);
+    const buttons = new Control(this.node);
+    const openButton = new Control(buttons.node, 'button', '', 'open');
+    openButton.node.onclick = ()=>{
+      this.onOpen();
+    }
   }
 
   update(data: IData){
@@ -74,6 +91,30 @@ class DataItemView extends Control{
   }
 }
 
+class PopUp extends Control{
+  private id: Control<HTMLElement>;
+  private userId: Control<HTMLElement>;
+  private title: Control<HTMLElement>;
+  private body: Control<HTMLElement>;
+
+  public onClose: ()=>void;
+
+  constructor(parentNode: HTMLElement, data:IData){
+    super(parentNode);
+    const dataContainer = new Control(this.node);
+    this.id = new Control(dataContainer.node, 'div', '', data.id.toString());
+    this.userId = new Control(dataContainer.node, 'div', '', data.userId.toString());
+    this.title = new Control(dataContainer.node, 'div', '', data.title);
+    this.body = new Control(dataContainer.node, 'div', '', data.body);
+
+    const buttons = new Control(this.node);
+    const closeButton = new Control(buttons.node, 'button', '', 'close');
+    closeButton.node.onclick = ()=>{
+      this.onClose();
+    }
+  }
+}
+
 export class Application extends Control{
   constructor(parentNode: HTMLElement){
     super(parentNode);
@@ -81,6 +122,13 @@ export class Application extends Control{
     model.load().then(loaded=>{
       loaded.data.map(itemData=>{
         const itemView = new DataItemView(this.node);
+        itemView.onOpen = ()=>{
+          //console.log(itemData);
+          const popup = new PopUp(this.node, itemData);
+          popup.onClose = ()=>{
+            popup.destroy();
+          }
+        }
         itemView.update(itemData);
       });
     })
